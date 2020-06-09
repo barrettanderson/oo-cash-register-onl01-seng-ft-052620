759e8090a1014abd56aeca496c034d5639af026c
Barrett Anderson <barrettanderson1989@gmail.com>
2020-06-09 18:27:51 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,10 +1,11 @@
 
 class CashRegister
-  attr_accessor :discount, :total
+  attr_accessor :discount, :total, :add_item
 
   def initialize(discount = nil)
     @total = 0
     @discount = discount
+    @total = total
   end
 
 
